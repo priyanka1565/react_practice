5bbae759f444c7f09ddcc9f8a22489604c681572
Sarsande-Sunil <priyankaingle250@gmail.com>
2023-03-04 17:28:18 +0530

    navbar added sucessfully

--- client/client/src/GlobalStyle.js
+++ client/client/src/GlobalStyle.js
@@ -157,7 +157,17 @@ input, textarea{
     cursor: pointer;
     }
 
- 
+ @media (max-width: ${({ theme }) => theme.media.tab}) {
+    .container {
+    max-width: 130rem;
+    padding: 0 3.2rem;
+  }
+  }
+
+   @media (max-width: ${({ theme }) => theme.media.mobile}) {
+       html {
+      font-size: 50%;
+    }
 
 .grid{
   gap: 3.2rem;
@@ -165,6 +175,6 @@ input, textarea{
       .grid-two-column , .grid-three-column, .grid-four-column{
           grid-template-columns: 1fr;
         }
-    
+    }
 
 `;
